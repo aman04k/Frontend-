59b80fcec9d688f999feb32ec53d060ce23dfce2
Aman Prajapti <amanprajapati7266@gmail.com>
2024-07-31 18:26:42 +0530

    add

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,27 @@
-let a = 5
-let b = 6
-let c = a +b
-console.log(c)
+// let a = 5
+// let b = 6
+// let c = a +b
+// console.log(c)
+
+// let a = 5
+// var b = "om"
+
+// // console.log(b)
+// const c = 45
+// // console.log(a)
+// var b = 40
+// console.log(b)
+
+
+// const c = "hello "
+// const f = "world"
+
+// console.log(c)
+
+let a = "hariom"
+
+{
+    let a = "om yadav"
+    // console.log(a)
+}
+console.log(a)
