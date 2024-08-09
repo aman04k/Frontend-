616473b1a359dcb8ea8142844a7ba10f0a4cea17
Aman <aman@grow4tech.com>
2024-08-09 17:21:56 +0530

    add

---
+++ JavaScript/DateTime/script.js
@@ -0,0 +1,12 @@
+// let date= new Date()
+// console.log(date)
+
+// let date = new Date( month, day, hours, minutes, seconds, milliseconds);
+// console.log(date)
+
+// let date = new Date(2024, 11);
+// console.log(date);
+
+// let date = new Date()
+// console.log(date.setDate(4,2,1))
+
