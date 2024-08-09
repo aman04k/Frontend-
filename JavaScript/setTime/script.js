a3a5b2c5ea9fb0c08ffff4bd684959962113dd3f
Aman <aman@grow4tech.com>
2024-08-09 17:48:04 +0530

    add

---
+++ JavaScript/setTime/script.js
@@ -0,0 +1,12 @@
+console.log("Hi");
+setTimeout(() => {
+	console.log("Let us see what happens");
+}, 2000);
+
+setTimeout(()=>{
+  console.log("this is nidhist")
+}, 10000)
+
+setTimeout(()=>{
+    console.log("this is om ")
+  }, 5000)
